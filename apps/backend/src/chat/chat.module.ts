9270af66cdfd74b90cf84c6755bfbf7f6be0cba5
TheRealSirHaXalot <sharp@electronics.no>
2025-11-21 15:37:40 +0100

    Fix chat module dependencies

--- apps/backend/src/chat/chat.module.ts
+++ apps/backend/src/chat/chat.module.ts
@@ -4,11 +4,12 @@ import { ConfigModule } from '@nestjs/config';
 import { ChatController } from './chat.controller';
 import { ChatMqttService } from './chat-mqtt.service';
 import { ChatService } from './chat.service';
+import { AuthModule } from '../auth/auth.module';
 import { MqttModule } from '../mqtt/mqtt.module';
 import { WsModule } from '../ws/ws.module';
 
 @Module({
-  imports: [ConfigModule, MqttModule, WsModule],
+  imports: [ConfigModule, AuthModule, MqttModule, WsModule],
   controllers: [ChatController],
   providers: [ChatService, ChatMqttService],
 })
